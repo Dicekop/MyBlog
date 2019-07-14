04d0da75e7d9f7f596f547ac061c3d112e46558b
EC2 Default User <ec2-user@ip-172-31-44-163.ap-northeast-1.compute.internal>
2019-07-14 12:08:40 +0000

    Add validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,7 @@ class Message < ApplicationRecord
     validates :title , length: { maximum: 32} , presence: true
     validates:message , length: { maximum: 256} , presence: true
     validates :name , length: { maximum: 32}
+    validates :weight , numericality: true
+    validates :height , numericality: true
     
 end
